886430559655d28e653d2adc301657ab6183f48e
Arman Baghdasaryan <armangr2022@gmail.com>
2022-08-11 14:36:31 +0400

    Books Management dded by maven

---
+++ src/main/java/model/Book.java
@@ -0,0 +1,22 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class Book {
+
+    private String title;
+    private Author author;
+    private int count;
+    private double price;
+    private String genre;
+
+    private User registeredUser;
+
+  }
---
+++ src/main/java/storage/UserStorage.java
@@ -0,0 +1,52 @@
+package storage;
+
+
+import model.User;
+
+public class UserStorage {
+
+    private User[] array = new User[10];
+    private int size;
+
+    public User add(User book) {
+        if (size == array.length) {
+            extendArray();
+
+        }
+        array[size++] = book;
+        return book;
+    }
+
+    private void extendArray() {
+        User[] temp = new User[array.length + 10];
+        System.arraycopy(array, 0, temp, 0, size);
+        array = temp;
+
+    }
+
+    public void print() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(i + "." + array[i]);
+
+
+        }
+
+    }
+    public User getUserByEmail(String email) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].getEmail().equals(email)) {
+                return array[i];
+
+            }
+        }
+        return null;
+    }
+
+}
+
+
+
+
+
+
+
